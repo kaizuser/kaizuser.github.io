3ea1ec683813224405be7bf646c944afd0d6e189
kaizuser <thiagochiesa4444@gmail.com>
2022-05-12 01:50:07 +0000

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+	return (
+		<>
+			<header className='flex justify-center items-center '>
+				<img src={require('../assets/kaizuserLogo.png')}/>
+
+				<nav>
+
+					<h1>Kaizuser</h1>
+				</nav>
+
+			</header>
+		</>
+
+	)
+}
