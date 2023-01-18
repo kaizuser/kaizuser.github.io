a9cc97c8b7d595e1f0a7616f89cb48b118ede836
kaizuser <thiagochiesa4444@gmail.com>
2023-01-18 05:53:23 -0300

    XP

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,19 @@ export default function () {
 					<h2 className='text-blueMain text-justify w-[4.5rem]'>&lt;XP&gt;</h2>
 
 					<div className='flex items-center max-w-[35rem] text-right flex-col text-justify'>
-						<p className='text-[#ffffff] text-xl m-3'>Coding bootcamp of more than 700 hours under the "learn by doing" methodology that simulates a demanding work environment in which various projects are developed using the scrum methodology</p>
+						<div className='flex items-center justify-center'>
+							<p className='text-blueMain'>-</p>
+							<p className='text-[#ffffff] text-xl m-3'>Coding bootcamp of more than 700 hours under the "learn by doing" methodology that simulates a demanding work environment in which various projects are developed using the scrum methodology</p>
+
+						</div>
+
+
+						<div className='flex items-center justify-center'>
+							<p className='text-blueMain'>-</p>
+							<p className='text-[#ffffff] text-xl m-3'>Freelance about an educative platform (check the github link in proyects section).</p>
+
+						</div>
+
 
 						<p className='text-[#ffffff] text-xl m-3'>Apart from that, I consider myself a supporter of the self-taught. I've been learning by my own methods for over 5 years in this amazing world. I've got solid bases in everything programation is refered to.</p>
 					</div>
